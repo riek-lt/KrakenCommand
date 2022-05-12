705947cfaaba89b071b988883876c924c4ccd975
Riekelt G <riekelt@hotmail.com>
2022-05-12 11:01:03 +0200

    Swapped quoationmarks (and backslash) with apostrophe

--- js/colorsdb.js
+++ js/colorsdb.js
@@ -1,14 +1,14 @@
-var rainbowBody = "{\"seg\": [{\"of\": 4, \"fx\": 110, \"sx\": 128, \"ix\": 128, \"pal\": 11}]}";
-var blueBody = "{\"seg\": [{\"fx\": 0, \"col\": [[0, 0, 255]]}]}";
-var cyanBody = "{\"seg\": [{\"fx\": 0, \"col\": [[0, 255, 200]]}]}";
-var greenBody = "{\"seg\": [{\"fx\": 0, \"col\": [[8, 255, 0]]}]}";
-var yellowBody = "{\"seg\": [{\"fx\": 0, \"col\": [[255, 200, 0]]}]}";
-var orangeBody = "{\"seg\": [{\"fx\": 0, \"col\": [[255, 160, 0]]}]}";
-var redBody = "{\"seg\": [{\"fx\": 0, \"col\": [[255, 0, 0]]}]}";
-var magentaBody = "{\"seg\": [{\"fx\": 0, \"col\": [[255, 0, 255]]}]}";
-var blackBody = "{\"seg\": [{\"fx\": 0, \"col\": [[0, 0, 0]]}]}";
+var rainbowBody = "{'seg': [{'of': 4, 'fx': 110, 'sx': 128, 'ix': 128, 'pal': 11}]}";
+var blueBody = "{'seg': [{'fx': 0, 'col': [[0, 0, 255]]}]}";
+var cyanBody = "{'seg': [{'fx': 0, 'col': [[0, 255, 200]]}]}";
+var greenBody = "{'seg': [{'fx': 0, 'col': [[8, 255, 0]]}]}";
+var yellowBody = "{'seg': [{'fx': 0, 'col': [[255, 200, 0]]}]}";
+var orangeBody = "{'seg': [{'fx': 0, 'col': [[255, 160, 0]]}]}";
+var redBody = "{'seg': [{'fx': 0, 'col': [[255, 0, 0]]}]}";
+var magentaBody = "{'seg': [{'fx': 0, 'col': [[255, 0, 255]]}]}";
+var blackBody = "{'seg': [{'fx': 0, 'col': [[0, 0, 0]]}]}";
 
-var redEyes = "{\"seg\": [{},{\"col\": [[255, 0, 0]]}]}";
-var whiteEyes = "{\"seg\": [{},{\"col\": [[255, 255, 255]]}]}";
-var greenEyes = "{\"seg\": [{},{\"col\": [[255, 255, 0]]}]}";
-var blackEyes = "{\"seg\": [{},{\"col\": [[0, 0, 0]]}]}";
+var redEyes = "{'seg': [{},{'col': [[255, 0, 0]]}]}";
+var whiteEyes = "{'seg': [{},{'col': [[255, 255, 255]]}]}";
+var greenEyes = "{'seg': [{},{'col': [[255, 255, 0]]}]}";
+var blackEyes = "{'seg': [{},{'col': [[0, 0, 0]]}]}";
