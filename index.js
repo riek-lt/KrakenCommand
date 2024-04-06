493ab6fafc75a857d04e0bced02eba0b3e2c46f4
Riekelt G <riekelt@hotmail.com>
2024-04-07 00:57:31 +0200

    This is pretty much it haha

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const LiveSplitClient = require('livesplit-client');
 const request = require('request');
 const colors = require('./colors/colors');
 const fs = require('fs');
+require('dotenv').config();
 
 var ComfyJS = require("comfy.js");
-ComfyJS.Init(process.env.TWITCHUSER, process.env.OAUTH);
+ComfyJS.Init('riekelt', process.env.OAUTH);
 
 var colorChoice;
 const eyesJSON = "colors/eyes.txt";
@@ -13,6 +14,8 @@ const bodyJSON = "colors/body.txt";
 
 var lastBodyColor = readData(bodyJSON);
 var lastEyesColor = readData(eyesJSON);
+console.log(lastBodyColor);
+var prevTime;
 
 
 
@@ -53,30 +56,39 @@ var lastEyesColor = readData(eyesJSON);
             try {
                 // Get current split name
                 const currentSplitName = await client.getCurrentSplitName();
-
+                const currentTimerPhase = await client.getCurrentTimerPhase();
                 // Check if split name has changed
                 if (currentSplitName !== previousSplitName) {
-                    // Display split name and delta
-                    const delta = await client.getDelta();
-                    console.log(`Split Name: ${currentSplitName}, Delta: ${delta}`);
-                    // Update previous split name
-                    previousSplitName = currentSplitName;
-                    const info = await client.getAll();
-					
-					//TODO: Distinguish between red and green
-					
-                    sendPost(colors.greenBody, "none");
+                    //See iff 
+                    if (currentTimerPhase == "Running") {
+                        // Display split name and delta
+                        const delta = await client.getDelta();
+                        console.log(`Split Name: ${currentSplitName}, Delta: ${delta}`);
+                        // Update previous split name
+                        previousSplitName = currentSplitName;
+
+                        //TODO: Distinguish between red and green
+                        const info = await client.getDelta();
+                        livesplitSignaler(paceChecker(info, prevTime));
+
+                        prevTime = info;
+
+                        //sendPost(colors.greenBody, "none");
+                        console.log('---------------------------------------------------------');
+                    } else {
+                        prevTime = "-0.00"
+                    }
                 }
             } catch (error) {
-                console.error('Error:', error);
+                console.error('Livesplit block Error:', error);
             }
-        }, 2000);
+        }, 5000);
 
         await client.getDelta();
 
 
     } catch (err) {
-        console.error(err); // Something went wrong
+        console.error("end of interval: " + err); // Something went wrong
     }
 })();
 
@@ -90,9 +102,9 @@ function sendPost(input, segment) {
         body: input
     }, (error, response, body) => {
         if (error) {
-            console.error('Error:', error);
+            console.error('sendPost Error:', error);
         } else if (response.statusCode !== 200) {
-            console.error('Error:', body);
+            console.error('Sendpost not 200 Error:', body);
         } else {
             switch (segment) {
                 case "body":
@@ -101,6 +113,9 @@ function sendPost(input, segment) {
                 case "eyes":
                     writeText(eyesJSON, input);
                     break;
+					default:
+					console.log('no write');
+					break;
             }
         }
     });
@@ -108,74 +123,168 @@ function sendPost(input, segment) {
 
 ComfyJS.onReward = (user, reward, cost, message, extra) => { //https://github.com/instafluff/ComfyJS?tab=readme-ov-file
     if (reward === "Squid eyes color") {
-            switch (message) {
-                case "white":
-                    colorChoice = colors.whiteEyes;
-                    break;
-                case "green":
-                    colorChoice = colors.greenEyes;
-                    break;
-                case "off":
-                    colorChoice = colors.blackEyes;
-                    break;
-                case "red":
-                    colorChoice = colors.redEyes;
-                    break;
-                case "magenta":
-                    colorChoice = colors.magentaEyes;
-                    break;
-            }
-            sendPost(colorChoice, 'eyes');
-            lastEyesColor = colorChoice;
+        switch (message) {
+            case "white":
+                colorChoice = colors.whiteEyes;
+                break;
+            case "green":
+                colorChoice = colors.greenEyes;
+                break;
+            case "off":
+                colorChoice = colors.blackEyes;
+                break;
+            case "red":
+                colorChoice = colors.redEyes;
+                break;
+            case "magenta":
+                colorChoice = colors.magentaEyes;
+                break;
         }
-        if (reward === "Squid color") {
-            switch (message) {
-                case "red":
-                    colorChoice = colors.redBody;
-                    break;
-                case "blue":
-                    colorChoice = colors.blueBody;
-                    break;
-                case "green":
-                    colorChoice = colors.greenBody;
-                    break;
-                case "white":
-                    colorChoice = colors.whiteBody;
-                    break;
-                case "yellow":
-                    colorChoice = colors.yellowBody;
-                    break;
-                case "orange":
-                    colorChoice = colors.orangeBody;
-                    break;
-                case "magenta":
-                    colorChoice = colors.magentaBody;
-                    break;
-                case "off":
-                    colorChoice = colors.blackBody;
-                    break;
-                case "cyan":
-                    colorChoice = colors.cyanBody;
-                    break;
-            }
-            sendPost(colorChoice, 'body');
-            lastBodyColor = colorChoice;
-        }
+        sendPost(colorChoice, 'eyes');
+        lastEyesColor = colorChoice;
     }
+    if (reward === "Squid color") {
+        switch (message) {
+            case "red":
+                colorChoice = colors.redBody;
+                break;
+            case "blue":
+                colorChoice = colors.blueBody;
+                break;
+            case "green":
+                colorChoice = colors.greenBody;
+                break;
+            case "white":
+                colorChoice = colors.whiteBody;
+                break;
+            case "yellow":
+                colorChoice = colors.yellowBody;
+                break;
+            case "orange":
+                colorChoice = colors.orangeBody;
+                break;
+            case "magenta":
+                colorChoice = colors.magentaBody;
+                break;
+            case "off":
+                colorChoice = colors.blackBody;
+                break;
+            case "cyan":
+                colorChoice = colors.cyanBody;
+                break;
+        }
+        sendPost(colorChoice, 'body');
+        lastBodyColor = colorChoice;
+    }
+}
 
+ComfyJS.onChat = (user, message, flags, self, extra) => {
+    if (user === "Riekelt") {
+        if (message === "party mode") {
+            sendPost(colors.partyMode, 'none');
+            setTimeout(function() {
+                sendPost(lastBodyColor, 'body');
+                sendPost(lastEyesColor, 'eyes');
+            }, 5000);
+        } else if (message === "full red") {
+            sendPost(colors.redBody, 'body');
+            sendPost(colors.redEyes, 'eyes');
+        }
+    } else {
+    }
+}
+
+function livesplitSignaler(signal) {
+    if (signal == "red") {
+        sendPost(colors.redBody, "none")
+    } else if (signal == "green") {
+        sendPost(colors.greenBody, "none")
+    }
+    setTimeout(function() {
+        sendPost(lastBodyColor, 'body');
+    //    sendPost(lastEyesColor, 'eyes');
+    }, 3000);
+}
+
+
+//function readData(filePath) {
+//    fs.readFile(filePath, 'utf8', (err, data) => {
+//        if (err) {
+//            console.error('Error reading file:', err);
+//        } 
+//		return data;
+//    });
+//}
 
 function readData(filePath) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-        } else {
-            return data;
-        }
-    });
+	try {
+  const data = fs.readFileSync(filePath, 'utf8');
+  console.log(data);
+  return data;
+} catch (err) {
+  console.error(err);
+}
 }
 
 function writeText(dest, content) {
     fs.writeFileSync(dest, content, (err) => {
         if (err) throw err;
     });
+}
+
+function paceChecker(currentTime, prevTime) {
+    const currentSeconds = timeParser(currentTime);
+    const prevSeconds = timeParser(String(prevTime));
+    const plusMinusCurrent = getPlusMinus(currentTime);
+    const plusMinusPrev = getPlusMinus(prevTime);
+
+
+    if ((currentTime !== undefined) || (prevTime !== undefined)) {
+        if ((plusMinusCurrent == '+') && (plusMinusPrev == "−")) {
+					console.log("I DID A 1 red");
+            return "red";
+        } else if ((plusMinusCurrent == '−') && (plusMinusPrev == "−")) {
+            if (currentSeconds - prevSeconds > 0) {
+							console.log("I DID A 2 green");
+                return "green";
+            } else {
+							console.log("I DID A 3 red");
+                return "red";
+            }
+        } else if ((plusMinusCurrent == '−') && (plusMinusPrev == "+")) {
+						console.log("I DID A 4 green");
+            return "green";
+        } else if ((plusMinusCurrent == '+') && (plusMinusPrev == "+")) {
+            if (currentSeconds - prevSeconds > 0) {
+							console.log("I DID A 5 red");
+                return "red";
+            } else {
+							console.log("I DID A 6 green");
+                return "green";
+            }
+        }
+    } else {
+        return "none";
+    }
+}
+
+function timeParser(time) {
+    if ((time.includes('+')) || (time.includes('−'))) {
+        var seconds = parseFloat('0.0');
+        time = time.substring(1)
+        if (time.includes(":")) {
+            var minuteGetter = time.split(":");
+            time = minuteGetter[1]
+            seconds += parseFloat(minuteGetter[0]) * 60;
+        }
+        seconds += parseFloat(time);
+        return seconds;
+    }
+}
+
+function getPlusMinus(time) {
+    if ((time.includes('−')) || (time.includes('+'))) {
+        const plusminus = time.charAt(0);
+        return plusminus
+    }
 }
