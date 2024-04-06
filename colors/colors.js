f82adeb1385e83b6401c4eeb14a41237f2452821
Riekelt G <riekelt@hotmail.com>
2024-04-06 12:42:24 +0200

    Translated Krakencommand to livesquid

---
+++ colors/colors.js
@@ -0,0 +1,22 @@
+const colors = {
+
+    rainbowBody: "{'seg': [{'of': 4, 'fx': 110, 'sx': 128, 'ix': 128, 'pal': 11}]}",
+    whiteBody: "{'seg': [{'fx': 0, 'col': [[255, 255, 255]]}]}",
+    blueBody: "{'seg': [{'fx': 0, 'col': [[0, 0, 255]]}]}",
+    cyanBody: "{'seg': [{'fx': 0, 'col': [[0, 255, 200]]}]}",
+    greenBody: "{'seg': [{'fx': 0, 'col': [[8, 255, 0]]}]}",
+    yellowBody: "{'seg': [{'fx': 0, 'col': [[255, 200, 0]]}]}",
+    orangeBody: "{'seg': [{'fx': 0, 'col': [[255, 160, 0]]}]}",
+    redBody: "{'seg': [{'fx': 0, 'col': [[255, 0, 0]]}]}",
+    magentaBody: "{'seg': [{'fx': 0, 'col': [[255, 0, 255]]}]}",
+    blackBody: "{'seg': [{'fx': 0, 'col': [[0, 0, 0]]}]}",
+
+    redEyes: "{'seg': [{},{'fx': 0, 'col': [[255, 0, 0]]}]}",
+    whiteEyes: "{'seg': [{},{'fx': 0, 'col': [[255, 255, 255]]}]}",
+    greenEyes: "{'seg': [{},{'fx': 0, 'col': [[255, 255, 0]]}]}",
+    magentaEyes: "{'seg': [{},{'fx': 0, 'col': [[255, 0, 255]]}]}",
+    blackEyes: "{'seg': [{},{'fx': 0, 'col': [[0, 0, 0]]}]}",
+
+    partyMode: "{'on':true,'bri':128,'transition':7,'mainseg':0,'seg':[{'id':0,'start':0,'stop':26,'grp':1,'spc':0,'of':4,'on':true,'frz':false,'bri':255,'cct':127,'n':'Squid Colour','col':[[255,255,255],[0,0,0],[0,0,0]],'fx':110,'sx':128,'ix':128,'pal':11,'sel':false,'rev':false,'mi':false},{'id':1,'start':26,'stop':32,'grp':1,'spc':0,'of':0,'on':true,'frz':false,'bri':255,'cct':127,'n':'Eyes','col':[[255,255,255],[0,0,0],[0,0,0]],'fx':25,'sx':255,'ix':128,'pal':0,'sel':true,'rev':false,'mi':false},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0},{'stop':0}]}"
+};
+module.exports = colors;
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 //pkg index.js --output livesquid
 const LiveSplitClient = require('livesplit-client');
 const request = require('request');
+const colors = require('./colors/colors');
+const fs = require('fs');
 
-var greenBody = "{'seg': [{'fx': 0, 'col': [[8, 255, 0]]}]}";
+var ComfyJS = require("comfy.js");
+ComfyJS.Init(process.env.TWITCHUSER, process.env.OAUTH);
+
+var colorChoice;
+const eyesJSON = "colors/eyes.txt";
+const bodyJSON = "colors/body.txt";
+
+var lastBodyColor = readData(bodyJSON);
+var lastEyesColor = readData(eyesJSON);
+
 
 
 (async () => {
@@ -34,8 +45,8 @@ var greenBody = "{'seg': [{'fx': 0, 'col': [[8, 255, 0]]}]}";
 
         // Connect to the server, Promise will be resolved when the connection will be succesfully established
         await client.connect();
-		
-		let previousSplitName = '';
+
+        let previousSplitName = '';
 
         // Function to check split name every 2000ms
         setInterval(async () => {
@@ -50,16 +61,18 @@ var greenBody = "{'seg': [{'fx': 0, 'col': [[8, 255, 0]]}]}";
                     console.log(`Split Name: ${currentSplitName}, Delta: ${delta}`);
                     // Update previous split name
                     previousSplitName = currentSplitName;
-        const info = await client.getAll();
-        console.log('Summary:', info);
-				sendPost(greenBody);
+                    const info = await client.getAll();
+					
+					//TODO: Distinguish between red and green
+					
+                    sendPost(colors.greenBody, "none");
                 }
             } catch (error) {
                 console.error('Error:', error);
             }
         }, 2000);
-		
-		await client.getDelta();
+
+        await client.getDelta();
 
 
     } catch (err) {
@@ -67,21 +80,102 @@ var greenBody = "{'seg': [{'fx': 0, 'col': [[8, 255, 0]]}]}";
     }
 })();
 
-function sendPost(input) {
+function sendPost(input, segment) {
     request.post({
         url: "http://192.168.2.31/json/state",
         headers: {
             'Content-Type': 'application/json',
             'Accept': "application/json"
         },
-		body: input
+        body: input
     }, (error, response, body) => {
         if (error) {
             console.error('Error:', error);
         } else if (response.statusCode !== 200) {
             console.error('Error:', body);
         } else {
-            // Do something with the successful response if needed
+            switch (segment) {
+                case "body":
+                    writeText(bodyJSON, input);
+                    break;
+                case "eyes":
+                    writeText(eyesJSON, input);
+                    break;
+            }
         }
+    });
+}
+
+ComfyJS.onReward = (user, reward, cost, message, extra) => { //https://github.com/instafluff/ComfyJS?tab=readme-ov-file
+    if (reward === "Squid eyes color") {
+            switch (message) {
+                case "white":
+                    colorChoice = colors.whiteEyes;
+                    break;
+                case "green":
+                    colorChoice = colors.greenEyes;
+                    break;
+                case "off":
+                    colorChoice = colors.blackEyes;
+                    break;
+                case "red":
+                    colorChoice = colors.redEyes;
+                    break;
+                case "magenta":
+                    colorChoice = colors.magentaEyes;
+                    break;
+            }
+            sendPost(colorChoice, 'eyes');
+            lastEyesColor = colorChoice;
+        }
+        if (reward === "Squid color") {
+            switch (message) {
+                case "red":
+                    colorChoice = colors.redBody;
+                    break;
+                case "blue":
+                    colorChoice = colors.blueBody;
+                    break;
+                case "green":
+                    colorChoice = colors.greenBody;
+                    break;
+                case "white":
+                    colorChoice = colors.whiteBody;
+                    break;
+                case "yellow":
+                    colorChoice = colors.yellowBody;
+                    break;
+                case "orange":
+                    colorChoice = colors.orangeBody;
+                    break;
+                case "magenta":
+                    colorChoice = colors.magentaBody;
+                    break;
+                case "off":
+                    colorChoice = colors.blackBody;
+                    break;
+                case "cyan":
+                    colorChoice = colors.cyanBody;
+                    break;
+            }
+            sendPost(colorChoice, 'body');
+            lastBodyColor = colorChoice;
+        }
+    }
+
+
+function readData(filePath) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+        } else {
+            return data;
+        }
+    });
+}
+
+function writeText(dest, content) {
+    fs.writeFileSync(dest, content, (err) => {
+        if (err) throw err;
     });
 }
