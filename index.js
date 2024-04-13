9be108f26839fe03dbbfd89c92d59897e7967bdb
riek-lt <riekelt@hotmail.com>
2024-04-13 02:50:59 +0200

    Added a lot of comments

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const colors = require('./colors/colors');
 const fs = require('fs');
 require('dotenv').config();
 
+//Integration of comfyjs is wonky and manual. See ComfyJS and limiter-async in node_modules
 var ComfyJS = require("comfy.js");
 ComfyJS.Init('riekelt', process.env.OAUTH);
 
 var colorChoice;
+var prevTime = "-0.00";
+
+//Load last colour into memory
 const eyesJSON = "colors/eyes.txt";
 const bodyJSON = "colors/body.txt";
-
-var prevTime = "-0.00";
 var lastBodyColor = readData(bodyJSON);
 var lastEyesColor = readData(eyesJSON);
 
@@ -52,15 +54,15 @@ var splitIndex = 0;
 
         let previousSplitName = '';
 
-        // Function to check split name every 2000ms
+        // Function to check split name every 1000ms
         setInterval(async () => {
             try {
-                // Get current split name
+                // Get current split name and phase (running?)
                 const currentSplitName = await client.getCurrentSplitName();
                 const currentTimerPhase = await client.getCurrentTimerPhase();
                 // Check if split name has changed
                 if (currentSplitName !== previousSplitName) {
-                    //See iff 
+                    //See if timer is running. If so do stuff.
                     if (currentTimerPhase == "Running") {
                         // Display split name and delta
                         const delta = await client.getDelta();
@@ -68,14 +70,17 @@ var splitIndex = 0;
                         // Update previous split name
                         previousSplitName = currentSplitName;
 						
+						//Save Delta, split number and BPT for gold, then give signals to lamp.
                         const info = await client.getDelta();
 						splitIndex = await client.getSplitIndex();
                         currentBPT = await client.getBestPossibleTime();
                         livesplitSignaler(paceChecker(info, prevTime));
-
+						
+						//Save time for new comparison
                         prevTime = info;
                         console.log('---------------------------------------------------------');
                     } else {
+						// If timer is not running, reset last time to 0.
                         prevTime = "-0.00"
                     }
                 }
@@ -84,7 +89,7 @@ var splitIndex = 0;
             }
         }, 1000);
 
-        await client.getDelta();
+        await client.getDelta();	//This probably could get deleted.
 
 
     } catch (err) {
@@ -92,6 +97,7 @@ var splitIndex = 0;
     }
 })();
 
+//Function for sending the API calls to the squid.
 function sendPost(input, segment) {
     request.post({
         url: "http://192.168.2.31/json/state",
@@ -107,6 +113,7 @@ function sendPost(input, segment) {
             console.error('Sendpost not 200 Error:', body);
         } else {
             switch (segment) {
+				//If second arg is body or eyes, save that string to files to remember what was last.
                 case "body":
                     writeText(bodyJSON, input);
                     break;
@@ -121,6 +128,7 @@ function sendPost(input, segment) {
     });
 }
 
+//Function for the rewards.
 ComfyJS.onReward = (user, reward, cost, message, extra) => { //https://github.com/instafluff/ComfyJS?tab=readme-ov-file
     if (reward === "Squid eyes color") {
         switch (message) {
@@ -178,6 +186,7 @@ ComfyJS.onReward = (user, reward, cost, message, extra) => { //https://github.co
     }
 }
 
+//currently only used for testing to not be reliant on rewards.
 ComfyJS.onChat = (user, message, flags, self, extra) => {
     if (user === "Riekelt") {
         if (message === "party mode") {
@@ -193,6 +202,8 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
     } else {}
 }
 
+//New sub enables party mode for 10 secs, then reverts
+//"none" means I'm not saving to eyes/body. Could be any word like banana
 ComfyJS.onSub = (user, message, subTierInfo, extra) => {
     console.log(user + ' subbed for months ' + subTierInfo + ' + ' + extra);
     sendPost(colors.partyMode, 'none');
@@ -203,6 +214,7 @@ ComfyJS.onSub = (user, message, subTierInfo, extra) => {
     }, 10000);
 }
 
+//Raids enable party mode for 10 seconds, then reverts
 ComfyJS.onRaid = (user, viewers, extra) => {
     sendPost(colors.partyMode, 'none');
     setTimeout(function() {
@@ -211,8 +223,12 @@ ComfyJS.onRaid = (user, viewers, extra) => {
     }, 10000);
 }
 
+//This function gets the colour the squid needs to be, and sends this as an API request.
 function livesplitSignaler(signal) {
+	//Triggers if BPT dropped, this means I golded. This is more important to check.
+	//Could probably be nested around the green signal instead of here.
     if (isGold()) {
+		//Unsure why this check is here.
 		if (splitIndex>0) {
 			sendPost(colors.liveGold, "none")
 		}
@@ -224,11 +240,12 @@ function livesplitSignaler(signal) {
         }
     }
     setTimeout(function() {
+		//After 5 seconds, return body. Since livesquid only affects the body, only this needs to be reset.
         sendPost(lastBodyColor, 'body');
-        //    sendPost(lastEyesColor, 'eyes');
     }, 5000);
 }
 
+//This function reads data from text files and returns it.
 function readData(filePath) {
     try {
         const data = fs.readFileSync(filePath, 'utf8');
@@ -238,24 +255,32 @@ function readData(filePath) {
     }
 }
 
+//Writes body/eyes to text file.
 function writeText(dest, content) {
     fs.writeFileSync(dest, content, (err) => {
         if (err) throw err;
     });
 }
 
+//This gets 2 livesplit values (ie "+2:55:21.22)" and compares the difference.
 function paceChecker(currentTime, prevTime) {
+	//Convert the string to seconds as a float (142.65 for 2:22.65)
     const currentSeconds = timeParser(currentTime);
     const prevSeconds = timeParser(String(prevTime));
+	//Saves the +/- if I'm behind or not. Needed for comparison.
     const plusMinusCurrent = getPlusMinus(currentTime);
     const plusMinusPrev = getPlusMinus(prevTime);
 
-
+//The next part compares the 2 times and judges whether I lost or gained time and returns green or red.
+//Only works if both aren't undefined. Doesn't work for the first split, need to look into that.
     if ((currentTime !== undefined) || (prevTime !== undefined)) {
+		//If went from - to +, this 100% means I lost time
         if ((plusMinusCurrent == '+') && (plusMinusPrev == "−")) {
             console.log("I DID A 1 red (ez)");
             return "red";
         } else if ((plusMinusCurrent == '−') && (plusMinusPrev == "−")) {
+			//If I stayed on - on both times, I can easily subtract current from prev. If positive, I gained time
+			//Likewise, if negative, it means I lost time. 
             if (currentSeconds - prevSeconds > 0) {
                 console.log("I DID A 2 green");
                 return "green";
@@ -263,6 +288,7 @@ function paceChecker(currentTime, prevTime) {
                 console.log("I DID A 3 red");
                 return "red";
             }
+			//Opposite logic from up here.
         } else if ((plusMinusCurrent == '−') && (plusMinusPrev == "+")) {
             console.log("I DID A 4 green (ez)");
             return "green";
@@ -280,24 +306,34 @@ function paceChecker(currentTime, prevTime) {
     }
 }
 
-function timeParser(time) { //add hrs
+//This function accepts a time format "+2:22:22.22" or "-11:11.11" and returns this in seconds (float) (671.11)
+function timeParser(time) { 
+	//Only works if string has a + or -. Makes things easier
     if ((time.includes('+')) || (time.includes('−'))) {
         var seconds = parseFloat('0.0');
+		//Get rid of plus or minus for calcs
         time = time.substring(1)
+		//If string has :, it at least has minutes added to it!
         if (time.includes(":")) {
+			//Make array split by hours (opt), minutes and (milli)seconds.
             var minuteGetter = time.split(":");
+			//If this is true, we got hours in the string.
             if (minuteGetter.length == 3) {
+				//Add hours in seconds, then get rid of index 0 so it's minutes and seconds left.
                 seconds += parseFloat(minuteGetter[0]) * 60 * 60;
                 minuteGetter = minuteGetter.slice(1);
             }
+			//Make time just seconds, then add minutes to seconds counter
             time = minuteGetter[1]
             seconds += parseFloat(minuteGetter[0]) * 60;
         }
+		//Add seconds to total and return. Conversion might not be needed anymore.
         seconds += parseFloat(time);
         return seconds;
     }
 }
 
+//Returns if it's a + or - at the start of a string. Input is "+2:22:22.22"
 function getPlusMinus(time) {
     if ((time.includes('−')) || (time.includes('+'))) {
         const plusminus = time.charAt(0);
@@ -305,11 +341,13 @@ function getPlusMinus(time) {
     }
 }
 
+//If the Best Possible Time changed this split, I golded. 
 function isGold() {
     if (timeParser("+" + currentBPT) < timeParser("+" + prevBPT)) {
         prevBPT = currentBPT;
         return true;
     } else {
+		//This is for initialisation?
         prevBPT = currentBPT;
         return false;
     }
